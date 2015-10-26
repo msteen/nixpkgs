35c6e93f7a1cd784e951a405d6ce07f0cd976741
Matthijs Steen <emailmatthijs@gmail.com>
2015-10-26 20:03:18 +0100

    audio-recorder: init at 1.7-5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10833,6 +10833,8 @@ let
     ffmpeg = ffmpeg_0;
   };
 
+  audio-recorder = callPackage ../applications/audio/audio-recorder { };
+
   milkytracker = callPackage ../applications/audio/milkytracker { };
 
   schismtracker = callPackage ../applications/audio/schismtracker { };
