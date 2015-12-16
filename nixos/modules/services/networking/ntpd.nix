aa619956ef5c40e75ca58f64e32f1158e22144b6
Matthijs Steen <matthijs@trans101.steen.network>
2015-12-16 23:35:55 +0100

    Updated the ntpd module to have additional options to make it more flexible.

--- nixos/modules/services/networking/ntpd.nix
+++ nixos/modules/services/networking/ntpd.nix
@@ -6,6 +6,8 @@ let
 
   inherit (pkgs) ntp;
 
+  cfg = config.services.ntp;
+
   stateDir = "/var/lib/ntp";
 
   ntpUser = "ntp";
@@ -16,10 +18,10 @@ let
     restrict 127.0.0.1
     restrict -6 ::1
 
-    ${toString (map (server: "server " + server + " iburst\n") config.services.ntp.servers)}
-  '';
+    ${toString (map (server: "server " + server + " iburst\n") cfg.servers)}
 
-  ntpFlags = "-c ${configFile} -u ${ntpUser}:nogroup";
+    ${cfg.extraConfig}
+  '';
 
 in
 
@@ -32,6 +34,7 @@ in
     services.ntp = {
 
       enable = mkOption {
+        type = types.bool;
         default = !config.boot.isContainer;
         description = ''
           Whether to synchronise your machine's time using the NTP
@@ -40,6 +43,7 @@ in
       };
 
       servers = mkOption {
+        type = types.listOf types.string;
         default = [
           "0.nixos.pool.ntp.org"
           "1.nixos.pool.ntp.org"
@@ -48,6 +52,30 @@ in
         ];
         description = ''
           The set of NTP servers from which to synchronise.
+        '';
+      };
+
+      enableIPv6 = mkOption {
+        type = types.bool;
+        default = config.networking.enableIPv6;
+        description = ''
+          Whether to enable support for IPv6.
+        '';
+      };
+
+      extraConfig = mkOption {
+        type = with types; lines;
+        default = "";
+        description = ''
+          Additional text appended to <filename>ntp.conf</filename>.
+        '';
+      };
+
+      extraOptions = mkOption {
+        type = with types; string;
+        default = "";
+        description = ''
+          Extra options used when launching ntpd.
         '';
       };
 
@@ -63,30 +91,29 @@ in
     # Make tools such as ntpq available in the system path.
     environment.systemPackages = [ pkgs.ntp ];
 
-    users.extraUsers = singleton
-      { name = ntpUser;
-        uid = config.ids.uids.ntp;
-        description = "NTP daemon user";
-        home = stateDir;
+    users.extraUsers = singleton {
+      name = ntpUser;
+      uid = config.ids.uids.ntp;
+      description = "NTP daemon user";
+      home = stateDir;
+    };
+
+    systemd.services.ntpd = {
+      description = "NTP Daemon";
+      wants = [ "network-online.target" ];
+      after = [ "dnsmasq.service" "bind.service" "network-online.target" ];
+      preStart = ''
+        mkdir -m 0755 -p ${stateDir}
+        chown ${ntpUser} ${stateDir}
+      '';
+      serviceConfig = {
+        ExecStart = "@${ntp}/bin/ntpd ntpd ${optionalString (!cfg.enableIPv6) "-4"} -g -c ${configFile} -u ${ntpUser}:nogroup ${cfg.extraOptions}";
+        Type = "forking";
       };
-
-    systemd.services.ntpd =
-      { description = "NTP Daemon";
-
-        wantedBy = [ "multi-user.target" ];
-
-        preStart =
-          ''
-            mkdir -m 0755 -p ${stateDir}
-            chown ${ntpUser} ${stateDir}
-          '';
-
-        serviceConfig = {
-          ExecStart = "@${ntp}/bin/ntpd ntpd -g ${ntpFlags}";
-          Type = "forking";
-        };
-      };
+      wantedBy = [ "multi-user.target" ];
+    };
 
   };
 
 }
+
